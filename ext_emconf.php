80947876a5158c578840c8fc8ff91d978f46c9d0
Alexander Schnitzler <git@alexanderschnitzler.de>
2017-12-13 16:17:36 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Composer Test',
+    'description' => '',
+    'category' => 'misc',
+    'author' => '',
+    'author_email' => '',
+    'author_company' => '',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => true,
+    'version' => '1.0.0.',
+    'constraints' => [
+        'depends' => [],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
